f0c672a9cc0ec9563d3dcde8a607eca6698fcd80
shafidark <safi29317@gmail.com>
2021-02-23 23:58:48 +0600

    first commit

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderMain from '../HeaderMain/HeaderMain';
+
+const Home = () => {
+    return (
+        <div>
+           <HeaderMain/> 
+        </div>
+    );
+};
+
+export default Home;
